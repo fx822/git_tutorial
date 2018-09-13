7fd3bb9433b4df435550a66037a931e1c8b09736
fx822 <fx822@hanmail.net>
2018-09-13 19:31:18 +0900

    Delete hello.py

--- hello.py
+++
@@ -1,9 +0,0 @@
-//For command line git tutorial
-// First : GitHub modification
-// Second : Local repository modification
-
-print("hello world")
-print("Tell Your World")
-print("Tell his world")
-print("Tell her world")
-print("test git pull")
